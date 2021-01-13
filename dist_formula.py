5ac853df599931460690fbcff4a3f5fc9cd96e12
Philip S Wong <shuihongwong@gmail.com>
2021-01-13 12:25:34 -0500

    update Poission Distribution

--- dist_formula.py
+++ dist_formula.py
@@ -1,18 +1,20 @@
 import math
 f_fact = lambda x : math.factorial(x)
-# binominal distribution
+# Binominal distribution
 prob_bin = lambda n, c, p: f_fact(n)/(f_fact(c) * f_fact(n-c)) * p**c * (1-p)**(n-c) 
 exp_bin = lambda n, p: n*p
 var_bin = lambda n, p: n*p*(1-p)
-# hypergeometric distribution
+# Hypergeometric distribution
 prob_hypergeom = lambda a, b, n, k: (f_fact(a)/(f_fact(a-k) *f_fact(k)) * f_fact(b)/(f_fact(n-k)*f_fact(b-n+k)))/(f_fact(a+b)/(f_fact(a+b-n)*f_fact(n)))
 exp_hypergeom = lambda a, b, n: n*(a/(a+b))
 var_hypergeom = lambda a, b, n: n*(a/a+b) *(1 - a/(a+b)) * ((a+b-n)/a+b-1)
-#geometric distribution
+# Geometric distribution
 prob_geom = lambda p, k: (1-p)**(k-1) * p
 exp_geom = lambda p: 1/p
 var_geom = lambda p: (1-p)/p**2
-#negative binominal distribution
+# Negative binominal distribution
 p_neg_bin = lambda k, r, p: f_fact(k-1)/(f_fact(k-r) * f_fact(r-1)) * p**r * (1-p)**(k-r)
 exp_neg_bin = lambda r, p: r/p
 var_neg_bin = lambda r, p: r*(1-p)/p**2
+# Poisson distribution
+
