f02c4d0fb4428f9931366ee12d175c87e0a59168
Philip S Wong <philipwong37@hotmail.com>
2021-01-12 23:10:33 -0500

    "adding hypergeometric dist"

--- dist_formula.py
+++ dist_formula.py
@@ -1,3 +1,8 @@
 import math
 f_fact = lambda x : math.factorial(x)
-f_bin = lambda n, c, p: f_fact(n)/(f_fact(c) * f_fact(n-c)) * p**c * (1-p)**(n-c) 
+# binominal distribution
+prob_bin = lambda n, c, p: f_fact(n)/(f_fact(c) * f_fact(n-c)) * p**c * (1-p)**(n-c) 
+exp_bin = lambda n, p: n*p
+var_bin = lambda n, p: n*p*q
+# hypergeometric distribution
+prob_hypergeom = lambda a, b, n, k: (f_fact(a)/(f_fact(a-k) *f_fact(k)) * f_fact(b)/(f_fact(n-k)*f_fact(b-n+k)))/(f_fact(a+b)/(f_fact(a+b-n)*f_fact(n)))  
