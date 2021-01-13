b2c1f74cd6e80df6cb2d8180d761b1b843a30cd1
Philip S Wong <philipwong37@hotmail.com>
2021-01-13 00:36:12 -0500

    added geometric distrib

--- dist_formula.py
+++ dist_formula.py
@@ -9,7 +9,7 @@ prob_hypergeom = lambda a, b, n, k: (f_fact(a)/(f_fact(a-k) *f_fact(k)) * f_fact
 exp_hypergeom = lambda a, b, n: n*(a/(a+b))
 var_hypergeom = lambda a, b, n: n*(a/a+b) *(1 - a/(a+b)) * ((a+b-n)/a+b-1)
 #geometric distribution
-f_geom = lambda p, k: (1-p)**(k-1) * p
+prob_geom = lambda p, k: (1-p)**(k-1) * p
 exp_geom = lambda p: 1/p
 var_geom = lambda p: (1-p)/p
 
