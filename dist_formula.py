91239e9924e03e3a6450ee20723f702e2154c901
Philip S Wong <shuihongwong@gmail.com>
2021-01-13 10:54:20 -0500

    updated binominal dist

--- dist_formula.py
+++ dist_formula.py
@@ -3,7 +3,7 @@ f_fact = lambda x : math.factorial(x)
 # binominal distribution
 prob_bin = lambda n, c, p: f_fact(n)/(f_fact(c) * f_fact(n-c)) * p**c * (1-p)**(n-c) 
 exp_bin = lambda n, p: n*p
-var_bin = lambda n, p: n*p*q
+var_bin = lambda n, p: n*p*(1-p)
 # hypergeometric distribution
 prob_hypergeom = lambda a, b, n, k: (f_fact(a)/(f_fact(a-k) *f_fact(k)) * f_fact(b)/(f_fact(n-k)*f_fact(b-n+k)))/(f_fact(a+b)/(f_fact(a+b-n)*f_fact(n)))
 exp_hypergeom = lambda a, b, n: n*(a/(a+b))
